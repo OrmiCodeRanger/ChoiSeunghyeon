589ae40c6adbef0b6049727133784923d8e93610
ChoiWheatley <chltmdgus604@naver.com>
2023-05-26 17:22:12 +0900

    [leet][787] ACCEPTED 💦💦💦

--- leet787.py
+++ leet787.py
@@ -2,50 +2,72 @@
 notice: 책에 있는 코드로는 더 이상 ACCEPT가 나오지 않는다. 다음 링크를 참조하여 더 효율적인 코드에 대한 토론을 확인해보자.
     https://github.com/onlybooks/algorithm-interview/issues/104
 """
-from collections import defaultdict
 from dataclasses import dataclass, field
 from heapq import heappush, heappop
 
 
 V = int  # vertex id
 W = int  # weight
+D = int  # depth
+
+INF = 1 << 30
 
 
 @dataclass(order=True)
 class VW:
     """shorthand for Vertex and Weight"""
 
-    vertex: int = field(compare=False)
-    weight: int
+    vertex: V = field(compare=False)
+    weight: W
+
+
+@dataclass
+class WD:
+    """shorthand for Weight and Depth"""
+
+    weight: W
+    depth: D
 
 
 @dataclass(order=True)
 class VWD(VW):
     """shorthand for Vertex, Weight, and Depth"""
 
-    depth: int = field(compare=False)
+    depth: D = field(compare=False)
 
 
 class Solution:
     def findCheapestPrice(
         self, n: int, flights: list[list[int]], src: int, dst: int, k: int
     ) -> int:
-        graph = defaultdict(list)
-        for u, v, w in flights:
-            graph[u].append((v, w))
+        # init graph aka adjacancy list
+        graph: list[list[VW]] = [[] for _ in range(n)]
+        for start, end, weight in flights:
+            graph[start].append(VW(end, weight))
 
-        # queue = [(weight, vertex, depth)]
-        queue = [(0, src, 0)]
+        # dist: 해당 노드를 방문했던 경로의 weight와 depth를 저장.
+        # 이때, depth가 작은 것을 우선한다.
+        dist: list[WD] = [WD(INF, 0)] * n
+
+        queue = [VWD(src, 0, 0)]
 
         while queue:
-            cur_w, cur_v, cur_k = heappop(queue)
-            if cur_v == dst:
-                return cur_w
-            if cur_k > k:
+            cur = heappop(queue)
+            if cur.vertex == dst:
+                return cur.weight
+            if cur.depth > k:
                 continue
-            # iterate neighbors
-            for neigh_v, neigh_w in graph[cur_v]:
-                alt_w = neigh_w + cur_w
-                alt_k = cur_k + 1
-                heappush(queue, (alt_w, neigh_v, alt_k))
+
+            for neighbor in graph[cur.vertex]:
+                # Ok, let's check we can push neighbors into the queue!
+                alt_weight = neighbor.weight + cur.weight
+                alt_depth = cur.depth + 1
+                if (
+                    alt_weight < dist[neighbor.vertex].weight
+                    or alt_depth <= dist[neighbor.vertex].depth
+                ):
+                    dist[neighbor.vertex] = WD(alt_weight, alt_depth)
+                    heappush(
+                        queue, VWD(neighbor.vertex, alt_weight, alt_depth)
+                    )
         return -1
