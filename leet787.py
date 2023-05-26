55329cc26c4666633d17983435fca9611635d958
ChoiWheatley <chltmdgus604@naver.com>
2023-05-26 16:16:16 +0900

    [leet][787] 아저씨 코드가 타임아웃이 나는 거였네;;;

--- leet787.py
+++ leet787.py
@@ -1,5 +1,8 @@
 """https://leetcode.com/problems/cheapest-flights-within-k-stops/
+notice: 책에 있는 코드로는 더 이상 ACCEPT가 나오지 않는다. 다음 링크를 참조하여 더 효율적인 코드에 대한 토론을 확인해보자.
+    https://github.com/onlybooks/algorithm-interview/issues/104
 """
+from collections import defaultdict
 from dataclasses import dataclass, field
 from heapq import heappush, heappop
 
@@ -27,37 +30,22 @@ class Solution:
     def findCheapestPrice(
         self, n: int, flights: list[list[int]], src: int, dst: int, k: int
     ) -> int:
-        """Dijkstra 정신을 활용한 문제풀이. K-stop 이내에 dst에 도착하는 최소거리
+        graph = defaultdict(list)
+        for u, v, w in flights:
+            graph[u].append((v, w))
 
-        Args:
-            n : number of vertices
-            flights : given graphs, [src, dst, weight]
-            k : within k-stops to arrive
-
-        Returns:
-            int: _description_
-        """
-        # convert flights into graph
-        graph: list[list[VW]] = [[] for _ in range(n)]
-        for start, end, weight in flights:
-            graph[start].append(VW(end, weight))
-
-        queue = [VWD(vertex=src, weight=0, depth=0)]
+        # queue = [(weight, vertex, depth)]
+        queue = [(0, src, 0)]
 
         while queue:
-            cur = heappop(queue)
-            if cur.vertex == dst:
-                return cur.weight
-            # 1순위: 경유지 count (depth)
-            if cur.depth > k:
+            cur_w, cur_v, cur_k = heappop(queue)
+            if cur_v == dst:
+                return cur_w
+            if cur_k > k:
                 continue
-            # 2순위: min weight -- 왜 아저씨 코드에는 minimum 계산이 없지?
-            for neighbor in graph[cur.vertex]:
-                alt = VWD(
-                    vertex=neighbor.vertex,
-                    weight=cur.weight + neighbor.weight,
-                    depth=cur.depth + 1,
-                )
-                heappush(queue, alt)
-
+            # iterate neighbors
+            for neigh_v, neigh_w in graph[cur_v]:
+                alt_w = neigh_w + cur_w
+                alt_k = cur_k + 1
+                heappush(queue, (alt_w, neigh_v, alt_k))
         return -1
