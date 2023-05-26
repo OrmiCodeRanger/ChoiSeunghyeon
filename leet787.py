647d42f4f44f96d355f910d17e3391c2f16cf060
ChoiWheatley <chltmdgus604@naver.com>
2023-05-26 15:57:24 +0900

    [leet][787] Time Limit Exceeded 💀

--- leet787.py
+++ leet787.py
@@ -1,24 +1,11 @@
 """https://leetcode.com/problems/cheapest-flights-within-k-stops/
 """
-from collections import defaultdict
 from dataclasses import dataclass, field
-from typing import Any, TypeVar, Generic
 from heapq import heappush, heappop
 
 
-T = TypeVar("T")
 V = int  # vertex id
 W = int  # weight
-
-INF = 1 << 30
-
-
-@dataclass(order=True)
-class Ord(Generic[T]):
-    """Custom comparable class"""
-
-    key: T = field(compare=True)
-    value: Any = field(compare=False)
 
 
 @dataclass(order=True)
@@ -36,43 +23,41 @@ class VWD(VW):
     depth: int = field(compare=False)
 
 
-def dijkstra(graph: list[list[VW]], source: V, max_depth: int) -> list[W]:
-    """
-    do dijkstra, within bounded depth
-    """
-    queue = [VWD(vertex=source, weight=0, depth=0)]
-    dist: list[W] = [INF] * len(graph)
-
-    while queue:
-        cur = heappop(queue)
-
-        if cur.depth > max_depth:
-            continue
-
-        dist[cur.vertex] = cur.weight
-        for neighbor in graph[cur.vertex]:
-            alt = VWD(
-                vertex=neighbor.vertex,
-                weight=cur.weight + neighbor.weight,
-                depth=cur.depth + 1,
-            )
-            heappush(queue, alt)
-
-    return dist
-
-
 class Solution:
     def findCheapestPrice(
         self, n: int, flights: list[list[int]], src: int, dst: int, k: int
     ) -> int:
+        """Dijkstra 정신을 활용한 문제풀이. K-stop 이내에 dst에 도착하는 최소거리
+
+        Args:
+            n : number of vertices
+            flights : given graphs, [src, dst, weight]
+            k : within k-stops to arrive
+
+        Returns:
+            int: _description_
+        """
         # convert flights into graph
         graph: list[list[VW]] = [[] for _ in range(n)]
         for start, end, weight in flights:
             graph[start].append(VW(end, weight))
 
-        # k is the number of stops, not all nodes!
-        distances = dijkstra(graph, src, k + 1)
+        queue = [VWD(vertex=src, weight=0, depth=0)]
 
-        if dst in distances:
-            return distances[dst]
+        while queue:
+            cur = heappop(queue)
+            if cur.vertex == dst:
+                return cur.weight
+            # 1순위: 경유지 count (depth)
+            if cur.depth > k:
+                continue
+            # 2순위: min weight -- 왜 아저씨 코드에는 minimum 계산이 없지?
+            for neighbor in graph[cur.vertex]:
+                alt = VWD(
+                    vertex=neighbor.vertex,
+                    weight=cur.weight + neighbor.weight,
+                    depth=cur.depth + 1,
+                )
+                heappush(queue, alt)
+
         return -1
